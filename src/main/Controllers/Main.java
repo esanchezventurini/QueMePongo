d0a76f1f0e8fbbb72cb7067f28878d9f862587b6
esanchezventurini <esanchezventurini@est.frba.utn.edu.ar>
2021-10-14 00:00:59 -0300

    Agrego aclaraciones sobre el contenido de los bodies

--- src/main/Controllers/Main.java
+++ src/main/Controllers/Main.java
@@ -12,7 +12,13 @@ public class Main {
     Spark.get("/usuarios/:id_usuario/prendas", prendaController::getPrendas);
     Spark.get("/usuarios/:id_usuario/prendas/:id_prenda", prendaController::getPrenda);
     Spark.delete("/usuarios/:id_usuario/prendas/:id_prenda", prendaController::deletePrenda);
+
+    //En el body deberá llegar un JSON con todos los atributos necesarios para instanciar una
+    //prenda, es decir, los que solicita su contructor.
     Spark.post("/usuarios/:id_usuario/prendas/:id_prenda", prendaController::createPrenda);
+
+    //Al ser un PUT y no un PATCH, en el body deberá venir el objeto completo con sus modificaciones,
+    //es decir, el mismo body que en el POST.
     Spark.put("/usuarios/:id_usuario/prendas/:id_prenda", prendaController::updatePrenda);
 
     Spark.get("/usuarios/:id_usuario/eventos", eventoController::getEventos);
